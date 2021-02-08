ea4a87c774ef71a772abc9244f675d79380d3b02
Grigoriy Safronov <gvsafronov@yandex.ru>
2021-02-08 20:08:29 +0300

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#define KEYDB_REAL_VERSION "1.0.1"
+#define KEYDB_REAL_VERSION "1.1.0"
 #define KEYDB_VERSION_NUM 0x00ffffff
 extern const char *KEYDB_SET_VERSION;   // Unlike real version, this can be overriden by the config
 
